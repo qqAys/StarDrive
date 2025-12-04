79adaaac29e62bc9210039bd118f6f35c507cc15
Jinx <me@qqays.xyz>
2025-12-04 17:30:15 +0800

    feat(utils): 实现动态多语言支持
    - 添加了对多种语言的动态加载支持
    - 实现了根据用户设置切换语言的功能
    - 增加了日志记录支持语言加载过程
    - 提供了默认语言回退机制
    - 改进了翻译函数以适应运行时环境
    - 从文件系统自动检测支持的语言列表

--- utils.py
+++ utils.py
@@ -1,14 +1,76 @@
 import gettext
 from logging import getLogger, StreamHandler
+from pathlib import Path
 
-lang = gettext.translation("messages", localedir="locales", languages=["en_US"])
-lang.install()
-_ = lang.gettext
+from nicegui import app
+
+# --- 日志 ---
 
 logger = getLogger("stardrive")
 logger.setLevel("DEBUG")
 logger.addHandler(StreamHandler())
 
+# --- 本地化 ---
+
+LOCALE_DIR = "locales"
+
+# 支持的语言列表
+# 从 locales 目录下加载
+SUPPORTED_LANGUAGES = [
+    item.name for item in Path(LOCALE_DIR).iterdir() if item.is_dir()
+]
+logger.debug(f"Supported languages: {SUPPORTED_LANGUAGES}")
+
+# 存储所有语言的 Translation 对象
+translations = {}
+
+
+def load_translations(
+        localedir=LOCALE_DIR, domain="messages", supported_languages=None
+):
+    """
+    加载所有支持语言的 Translation 对象到字典中。
+    """
+    if supported_languages is None:
+        supported_languages = SUPPORTED_LANGUAGES
+    for lang_code in supported_languages:
+        try:
+            # 创建特定语言的 Translation 对象
+            t = gettext.translation(domain, localedir=localedir, languages=[lang_code])
+            translations[lang_code] = t
+        except FileNotFoundError:
+            logger.warning(f"Translation file not found for language: {lang_code}")
+            translations[lang_code] = gettext.NullTranslations()
+
+
+load_translations()
+
+
+def dynamic_gettext(message: str, lang_code: str = None) -> str:
+    """
+    根据给定的语言代码返回翻译后的消息。
+    """
+    if lang_code is None:
+        try:
+            # 尝试从 NiceGUI 用户存储中获取
+            lang_code = app.storage.user.get("default_lang", "en_US")
+        except RuntimeError:
+            # 如在初始化时，使用默认语言
+            lang_code = "en_US"
+    # 查找对应的 Translation 对象
+    translator = translations.get(lang_code)
+
+    if translator:
+        # 使用该对象的 gettext 方法进行翻译
+        return translator.gettext(message)
+    else:
+        return message
+
+
+_ = dynamic_gettext
+
+
+# --- 工具函数 ---
 
 def bytes_to_human_readable(num_bytes: int) -> str:
     """
