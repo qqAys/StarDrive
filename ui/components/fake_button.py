57523bd773fa4b96f22faaf82ba5be48af68a37c
Jinx <me@qqays.xyz>
2025-12-09 16:31:55 +0800

    feat(ui): 自适应隐藏登出按钮

--- ui/components/fake_button.py
+++ ui/components/fake_button.py
@@ -2,7 +2,6 @@ from typing import Callable, Any
 
 from nicegui import ui
 
-
 fake_button_style = "text-white font-medium hover:bg-white/20 rounded ring-1 ring-white/10 px-2 py-1 transition-colors duration-200"
 
 
@@ -12,7 +11,7 @@ def fake_button(text: str, icon: str = None, on_click: Callable[[], Any] = None)
         with ui.row().classes("items-center"):
             if icon:
                 ui.icon(icon)
-            ui.label(text)
+            ui.label(text).classes("gt-sm")
 
     b.on("click", on_click)
 
